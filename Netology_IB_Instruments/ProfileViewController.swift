a6ebb46d71bbcd4625080dedd47bc9a4ade8a62f
Artem Sviridov <exastronaut@yandex.ru>
2022-02-23 22:00:56 +0300

    the task completed

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Артем Свиридов on 23.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+
+        if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 16, y: 50, width: screenWidth - 32, height: screenHeight - 100)
+            view.addSubview(profileView)
+        }
+    }
+}
