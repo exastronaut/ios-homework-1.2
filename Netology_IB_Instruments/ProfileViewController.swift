775fa893a4c4025ac7a10c1424aed3a0e2697b1a
Artem Sviridov <exastronaut@yandex.ru>
2022-03-03 19:20:32 +0300

    edited height a frame for profileView

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -16,7 +16,7 @@ class ProfileViewController: UIViewController {
         let screenHeight = UIScreen.main.bounds.height
 
         if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
-            profileView.frame = CGRect(x: 16, y: 50, width: screenWidth - 32, height: screenHeight - 400)
+            profileView.frame = CGRect(x: 16, y: 50, width: screenWidth - 32, height: screenHeight - 100)
             view.addSubview(profileView)
         }
     }
